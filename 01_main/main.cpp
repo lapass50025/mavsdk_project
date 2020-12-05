33415a61f565ac4404309393b230d343a052bfe2
lapass50025 <lapss50025@gmail.com>
2020-12-05 20:35:02 +0900

    main function

--- 01_main/main.cpp
+++ 01_main/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-#include <stdio.h>
+#include <thread>
+#include <chrono>
+#include <mavsdk/mavsdk.h>
+#include <mavsdk/plugins/action/action.h>
+#include <mavsdk/plugins/telemetry/telemetry.h>
+#include <mavsdk/plugins/mission/mission.h>
+#include <future>
 
-using namespace std;
+using namespace mavsdk;
+
+
 
 int main(void)
 {
-    printf("Hello, World!!!");
     return 0;
 }
